5fd88abe8413a0f9e1bb15cfc20b617c883bf6bb
Anders Ågren <anders.agren@rssoftware.se>
2018-12-11 11:56:52 +0100

    WIP

--- EcWamp/Program.cs
+++ EcWamp/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExoConfig;
+using ExoConfig.Query;
+using ExoConfig.Support;
 
 namespace EcWamp
 {
@@ -10,6 +13,17 @@ namespace EcWamp
     {
         static void Main(string[] args)
         {
+            EXO.DomainCx domain=new EXO.DomainCx();
+            domain.Domain = new EXO.DomainCx.tAreaDomain(@"C:\EXO Projects\Regin\Styrsystem1");
+            string defaultController=ExoProjectSupport.GetDefaultController(@"C:\EXO Projects\Regin\Styrsystem1");
+            EcQuery ecQuery = new EcQuery("esav");
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("Controller", defaultController);
+            dict.Add("UnnamedArgument_2", "Detta är arg 2!");
+            EcDataSet dataSet = ecQuery.GetData(@"C:\EXO Projects\Regin\Styrsystem1\EXOFlex_1.Esav", dict, domain);
+            
+            Console.ReadKey();
+            
         }
     }
 }
