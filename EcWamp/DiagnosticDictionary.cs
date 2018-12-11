9a590493b62f75cc152b42434124aa794e6d4aaa
Anders Ågren <anders.agren@rssoftware.se>
2018-12-11 15:20:02 +0100

    WIP

---
+++ EcWamp/DiagnosticDictionary.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+
+namespace EcWamp
+{
+    /// <summary>
+    /// A dictionary with an indexer that produces an informative
+    /// KeyNotFoundException message.
+    /// </summary>
+
+    public class DiagnosticDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        protected object tag;
+        protected string name = "unknown";
+
+        /// <summary>
+        /// Gets/sets an object that you can associate with the dictionary.
+        /// </summary>
+        /// De
+        public object Tag
+        {
+            get { return tag; }
+            set { tag = value; }
+        }
+
+        /// <summary>
+        /// The dictionary name. The default is "unknown".
+        /// Used to enhance the KeyNotFoundException.
+        /// </summary>
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// Parameterless constructor.
+        /// </summary>
+        public DiagnosticDictionary()
+        {
+        }
+
+        public DiagnosticDictionary(IEqualityComparer<TKey> comp) : base(comp)
+        {
+        }
+
+        /// <summary>
+        /// Constructor that takes a name.
+        /// </summary>
+        public DiagnosticDictionary(string name)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Constructor that takes a name.
+        /// </summary>
+        public DiagnosticDictionary(string name, IEqualityComparer<TKey> comp) : base(comp)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Indexer that produces a more useful KeyNotFoundException.
+        /// </summary>
+        public new TValue this[TKey key]
+        {
+            get
+            {
+                try
+                {
+                    return base[key];
+                }
+                catch (KeyNotFoundException)
+                {
+                    throw new KeyNotFoundException("The key '" + key.ToString() +
+                       "' was not found in the dictionary '" + name + "'. in method: " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+                }
+            }
+
+            set { base[key] = value; }
+        }
+
+        public void Add(Dictionary<TKey, TValue> indict)
+        {
+            foreach (KeyValuePair<TKey, TValue> kvp in indict)
+            {
+                Add(kvp.Key, kvp.Value);
+            }
+        }
+    }
+}
--- EcWamp/Program.cs
+++ EcWamp/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ExoConfig;
 using ExoConfig.Query;
 using ExoConfig.Support;
+using EXO;
 
 namespace EcWamp
 {
@@ -13,8 +15,8 @@ namespace EcWamp
     {
         static void Main(string[] args)
         {
-            EXO.DomainCx domain=new EXO.DomainCx();
-            domain.Domain = new EXO.DomainCx.tAreaDomain(@"C:\EXO Projects\Regin\Styrsystem1");
+            DomainCx domain =new DomainCx();
+            domain.Domain = new DomainCx.tAreaDomain(@"C:\EXO Projects\Regin\Styrsystem1");
             string defaultController=ExoProjectSupport.GetDefaultController(@"C:\EXO Projects\Regin\Styrsystem1");
             EcQuery ecQuery = new EcQuery("esav");
             Dictionary<string, string> dict = new Dictionary<string, string>();
@@ -24,6 +26,82 @@ namespace EcWamp
             
             Console.ReadKey();
             
+        }
+
+        public static void TT(string filepath, DomainCx domain, string defaultController)
+        {
+            //string filepath = "";
+            var esavFormat = new EcQuery("Esav");
+            //if (!EXO.EXOlib.CheckIdentifier(parser.ViewName))
+            //{
+            //    throw new BadIdentifierException(parser.ViewName, null);
+            //}
+
+
+            //filepath = FileOp.TranslatePath(parser.Path, parent);
+            FileInfo f = new FileInfo(filepath);
+            if (!f.Exists)
+            {
+                throw new FileNotFoundException("The call has an invalid File parameter. The file does not exist. (" + filepath + ")");
+            }
+
+            var argumentDataSet = esavFormat.GetData(filepath, new DiagnosticDictionary<string, string>(), domain);
+
+            Dictionary<string, string> runtimeArgs = new Dictionary<string, string>();
+            string controllerMacro = defaultController;
+            runtimeArgs["controller"] = controllerMacro;
+            //runtimeArgs["account"] = DataItemFactory.GetAccountMacro(parent);
+
+            if (argumentDataSet.Result == DataSetResults.Success)
+            {
+                if (argumentDataSet.Nodes[0] != null && argumentDataSet.Nodes[0].Children[0] != null)
+                {
+                    int i = 0;
+                    var arguments = parser.Arguments.ToList();
+                    foreach (EcDataNode ecDataNode in argumentDataSet.Nodes[0].Children[0].Children)
+                    {
+                        //fetch argument names
+                        if (i >= arguments.Count) break;
+
+                        string name = ecDataNode.GetProperty("Name").Value;
+                        string defaultValue = ecDataNode.GetProperty("Default").Value;
+
+                        string runtimeValue = i < arguments.Count ? arguments[i].Value : defaultValue;
+
+                        if (runtimeValue.ToLower() == "(default)")
+                        {
+                            runtimeValue = defaultValue;
+                        }
+                        if (runtimeArgs.ContainsKey(name)) continue;
+                        runtimeArgs[name] = runtimeValue;
+                        i++;
+                    }
+                }
+            }
+            else
+            {
+                throw new FileInBadFormatException(filepath, argumentDataSet.Result, null);
+            }
+
+            // var dataSet = EsavFormat.GetData(filepath, runtimeArgs, FileOp.GetDomain(parent));
+            //EcQuery.CollectTimerEvents = true;
+            var dataSet = esavFormat.GetData(filepath, runtimeArgs, FileOp.GetDomain(parent));
+            //events.AddRange(EcQuery.TimerEntries.Select((entry => new ExoWebSession.WatchEvent { EventText = entry.EventText, ElapsedMs = entry.ElapsedMs })));
+            //EcQuery.TimerEntries = new List<EcQuery.TimerEntry>();
+            //EcQuery.CollectTimerEvents = false;
+
+            //arguments is 
+
+            //events.Add(new ExoWebSession.WatchEvent
+            //{
+            //    EventText = "Read file content with arguments"+filepath,
+            //    ElapsedMs = sw.ElapsedMilliseconds
+            //}); sw.Restart();
+
+            //StaticTimer.Stop(701);
+
+            if (dataSet.Result != DataSetResults.Success)
+                return null;
         }
     }
 }
