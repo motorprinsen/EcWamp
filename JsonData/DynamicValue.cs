15cc1cc0d4c5ca6e976776b4afa57725411c4dce
Daniel Lindström <daniel.lindstrom@regin.se>
2018-12-16 15:09:40 +0100

    Add procedure class

---
+++ JsonData/DynamicValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JsonData
+{
+    public class DynamicValue
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public String ChildIndex { get; set; }
+    }
+    
+
+}
---
+++ JsonData/WampProcedure.cs
@@ -0,0 +1,11 @@
+﻿
+using WampSharp.V2.Rpc;
+
+namespace JsonData
+{
+    public interface IViewService
+    {
+        [WampProcedure("com.arguments.getView")]
+        JsonDataSet GetView(string area, string viewFile, object[] args);
+    }
+}
